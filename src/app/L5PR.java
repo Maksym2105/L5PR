2612de838c29d3c7345c9fd1d7b2302eca058403
Maksym2105 <maxdetrai02@gmail.com>
2024-12-13 10:02:18 +0200

    Added findUnique

--- src/app/L5PR.java
+++ src/app/L5PR.java
@@ -7,39 +7,52 @@ import java.util.List;
 public class L5PR {
     public static void main(String[] args) {
 
-        List<String> list = List.of(
+        List<String> petsList = List.of(
                 "Cat" , "Dog" ,"Bird" , "Hamster" , "Lizard" ,
                 "Rabbit" , "Rat" , "Dog" , "Fish" , "Bird" );
 
-        System.out.println(list);
-
+        System.out.println(petsList);
         String strDog = "Dog";
-        System.out.println(countOccurrences(list , strDog));
+        System.out.println(countOccurrences(petsList, strDog));
 
-        int[] numbers = {1, 2, 3};
+        int[] array = {1, 2, 3};
+        System.out.println("Array" + Arrays.toString(array));
+        System.out.println("List" + toList(array));
 
-        System.out.println("Array" + Arrays.toString(numbers));
-        System.out.println("List" + toList(numbers));
-
+        List<Integer> numbers = Arrays.asList(1,2,1,3,4,4,5,2,4,2);
+        List<Integer> uniqueNumbers = findUnique(numbers);
+        System.out.println(uniqueNumbers);
 
         }
-        public static int countOccurrences(List<String> list, String str) {
+        public static int countOccurrences(List<String> petsList, String strDog) {
         int count = 0;
-        for (int i = 0; i < list.size(); i++) {
-            if (list.get(i).equals(str)) {
+        for (int i = 0; i < petsList.size(); i++) {
+            if (petsList.get(i).equals(strDog)) {
                 count++;
             }
         }
         return count;
         }
 
-        public static List<Integer> toList(int[] numbers) {
+        public static List<Integer> toList(int[] array) {
         List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < numbers.length; i++) {
-            list.add(numbers[i]);
+        for (int i = 0; i < array.length; i++) {
+            list.add(array[i]);
         }
         return list;
         }
+
+        public static List<Integer> findUnique(List<Integer> numbers) {
+        List<Integer> uniqueNumbers = new ArrayList<>();
+
+        for(Integer number : numbers) {
+            if(!uniqueNumbers.contains(number)){
+                uniqueNumbers.add(number);
+            }
+        }
+        return uniqueNumbers;
+        }
+
 
 
     }
